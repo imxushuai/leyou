e8132819eea912be233ec2796dbda32e9c8b8c61
xushuai <xs_fight@163.com>
2019-03-28 15:46:58 +0800

    add LICENSE
    - 新增license，并对项目进行license署名

--- leyou-portal/js/plugins/citypicker/distpicker.js
+++ leyou-portal/js/plugins/citypicker/distpicker.js
@@ -1,13 +1,18 @@
-/*!
- * Distpicker v1.0.4
- * https://github.com/fengyuanchen/distpicker
+/*
+ * Copyright © 2019-2019 imxushuai
  *
- * Copyright (c) 2014-2016 Fengyuan Chen
- * Released under the MIT license
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Date: 2016-06-01T15:05:52.606Z
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as anonymous module.
