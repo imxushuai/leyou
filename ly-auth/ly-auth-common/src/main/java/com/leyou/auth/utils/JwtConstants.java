225e964164291ce3f6692278056fdc02b1d6ec6b
xushuai <xs_fight@163.com>
2019-05-26 23:30:19 +0800

    Add 授权中心初步搭建完成（待完善api）

---
+++ ly-auth/ly-auth-common/src/main/java/com/leyou/auth/utils/JwtConstants.java
@@ -0,0 +1,9 @@
+package com.leyou.auth.utils;
+
+/**
+ * jwt常量类
+ */
+public class JwtConstants {
+    public static final String JWT_KEY_ID = "id";
+    public static final String JWT_KEY_USER_NAME = "username";
+}
